c3a982e898b647f8bec2454bf2af23760a1cb93c
Abraham <abraham@abranhe.com>
2023-05-19 22:53:19 -0400

    refactor: reorganize exports in .exports file

--- email_setup.sh
+++ email_setup.sh
@@ -4,9 +4,9 @@ HOOK_NAME=$(basename "$0")
 repo_path=$(git rev-parse --show-toplevel)
 
 if [[ "$repo_path" == *"/dev/github/"* ]]; then
-  git config --local user.email $(op read "op://excel/me/git/email")
-elif [[ "$repo_path" == *"/dev/gitlab/"* ]]; then
   git config --local user.email $(op read "op://personal/me/git/email")
+elif [[ "$repo_path" == *"/dev/gitlab/"* ]]; then
+  git config --local user.email $(op read "op://excel/me/git/email")
 fi
 
 # If there is local hook, run it
